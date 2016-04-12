a46b1460a79ba81b0381ced6e6fcc5444296e5be
Jordi Ortolá Ankum <jordi665@hotmail.com>
2016-04-12 20:41:11 +0200

    Fixed global dummyAudio var

--- assets/js/common/featureDetection.js
+++ assets/js/common/featureDetection.js
@@ -2,7 +2,7 @@ Modernizr.addTest(
   'AudioPlayOnlyOnGesture',
 
   function() {
-    dummyAudio = new Audio(MEXICANS_AUDIO_URL);
+    var dummyAudio = new Audio(MEXICANS_AUDIO_URL);
     dummyAudio.play();
     var canPlay = dummyAudio.paused;
     dummyAudio.pause();
